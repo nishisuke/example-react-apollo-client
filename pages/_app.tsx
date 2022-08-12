5ecbdf0ee6068e939eef3e0d74b2b64101d84ed4
nishisuke <ryoskns+git@gmail.com>
2022-08-12 21:19:19 +0900

    prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/query',
+  uri: "http://localhost:8080/query",
   cache: new InMemoryCache(),
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <ApolloProvider client={client}>
-<Component {...pageProps} />
-  </ApolloProvider>
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,20 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
-import {TodoDocument} from '../graphql/generated'
-import {  useQuery } from '@apollo/client'
+import { TodoDocument } from "../graphql/generated";
+import { useQuery } from "@apollo/client";
 
- 
 const Home: NextPage = () => {
-  const { data,loading } = useQuery(TodoDocument, {variables: {first:4}})
+  const { data, loading } = useQuery(TodoDocument, { variables: { first: 4 } });
 
-if (loading) return <div>Loading</div>
-
-
-
-
-
-
+  if (loading) return <div>Loading</div>;
 
   return (
-<>
-{data?.todos.edges.map((e) => <div key={e.node.id}>
-{e.node.text}
-</div>)}
-</>
-  )
+    <>
+      {data?.todos.edges.map((e) => (
+        <div key={e.node.id}>{e.node.text}</div>
+      ))}
+    </>
+  );
+};
 
-}
-
-export default Home
+export default Home;
